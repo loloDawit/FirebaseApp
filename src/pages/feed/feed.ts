d53c743c9d4335693935550569e5a2ff3350f297
Dawit Abera <dawitabera@Dawits-iMac.local>
2018-08-06 19:40:51 -0700

    added firestore functionality

--- src/pages/feed/feed.ts
+++ src/pages/feed/feed.ts
@@ -1,18 +1,34 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
+import firebase from 'firebase';
 
 @Component({
   selector: 'page-feed',
   templateUrl: 'feed.html',
 })
 export class FeedPage {
+  text:string="";
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad FeedPage');
+  }
+  postUpdate(){
+    firebase.firestore().collection("posts").add({
+      text: this.text,
+      created: firebase.firestore.FieldValue.serverTimestamp,
+      owner: firebase.auth().currentUser.uid,
+      owner_name:firebase.auth().currentUser.displayName
+    }).then((doc) => {
+      console.log(doc);
+      
+    }).catch((err) =>{
+      console.log(err);
+      
+    })
   }
 
 }
