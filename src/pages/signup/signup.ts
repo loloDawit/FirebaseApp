fb3db4c6f5785f3e9fbd43ec65ecbef8d4e97240
Dawit Abera <dawitabera@Dawits-iMac.local>
2018-08-08 17:55:56 -0700

    fixed display name issue

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -13,6 +13,7 @@ export class SignupPage {
   name:string = "";
   email:string = "";
   password:string = "";
+ 
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public toastCtrl:ToastController, public alertCtrl:AlertController) {
   }
@@ -30,48 +31,51 @@ export class SignupPage {
     firebase.auth().createUserWithEmailAndPassword(this.email,this.password).then((userData)=>{
       
       let newUser: firebase.User = userData.user;
-
-      newUser.updateProfile({
-        displayName: this.name,
-        photoURL: ""
-      }).then(() =>{
-        console.log("profile updated");
-
-        this.alertCtrl.create ({
-          title: "Account Created", 
-          message: "Your account has been created successfully.",
-          buttons: [
-            {
-              text: "Ok",
-              handler: () =>{
-                // Navigate to new page
-                this.navCtrl.setRoot(FeedPage);
-              }
-            }
-          ]
-        }).present();
-        
-      }).catch((err) =>{
-        this.toastCtrl.create({
-          message: err.message,
-          duration: 3000
-  
-        }).present();
-        console.log(err);
-        
-      })
-      console.log(userData.user.displayName)
+      this.getUser(newUser);
+      
+      
     }).catch((error)=>{
       console.log(error);
       
     })
     console.log(this.email + this.name + this.password);
-    this.clear();
+    //this.clear();
   }
   clear(){
     this.name="";
     this.email="";
     this.password="";
   }
+  getUser(user){
+    firebase.auth().currentUser;
+    user.updateProfile({
+      displayName: this.name,
+      photoURL: ""
+    }).then(()=>{
+      console.log("profile updated");
 
+  this.alertCtrl.create ({
+    title: "Account Created", 
+    message: "Your account has been created successfully.",
+    buttons: [
+      {
+        text: "Ok",
+        handler: () =>{
+          // Navigate to new page
+          this.navCtrl.setRoot(FeedPage);
+        }
+      }
+    ]
+  }).present();
+  
+}).catch((err) =>{
+  this.toastCtrl.create({
+    message: err.message,
+    duration: 3000
+
+  }).present();
+  console.log(err);
+
+    });
+  }
 }
