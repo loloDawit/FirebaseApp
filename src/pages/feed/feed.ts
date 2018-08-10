37b2e13c876daec19eea9e3c02bd48722aa98922
Dawit Abera <dawitabera@Dawits-iMac.local>
2018-08-10 11:11:42 -0700

    pagination added to load specific number of pages per scroll

--- src/pages/feed/feed.ts
+++ src/pages/feed/feed.ts
@@ -1,66 +1,104 @@
-import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { Component } from "@angular/core";
+import { NavController, NavParams } from "ionic-angular";
 
-import firebase from 'firebase';
-import { LoginPage } from '../login/login';
+import firebase from "firebase";
+import { LoginPage } from "../login/login";
 
-import moment from 'moment';
-
+import moment from "moment";
 
 @Component({
-  selector: 'page-feed',
-  templateUrl: 'feed.html',
+  selector: "page-feed",
+  templateUrl: "feed.html"
 })
 export class FeedPage {
-  text:string="";
+  text: string = "";
   posts: any[] = [];
-
+  pageSize: number = 5;
+  cursor: any;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.getPosts();
   }
   //get posts from the cloud
-  getPosts(){
-    this.posts = []
-    firebase.firestore().collection("posts").orderBy("created","desc").get()
-    .then((data) =>{  // re-arrange posts by descending order
+  getPosts() {
+    this.posts = [];
+    firebase
+      .firestore()
+      .collection("posts")
+      .orderBy("created", "desc")
+      .limit(this.pageSize)
+      .get()
+      .then(data => {
+        // re-arrange posts by descending order
 
-      data.forEach((document) => {
-        this.posts.push(document);
+        data.forEach(document => {
+          this.posts.push(document);
+        });
+        console.log(this.posts);
+        this.cursor = this.posts[this.posts.length -1];
       })
-      console.log(this.posts);
-      
-    }).catch((err) =>{
-      console.log(err);
-      
-    });
+      .catch(err => {
+        console.log(err);
+      });
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad FeedPage');
+    console.log("ionViewDidLoad FeedPage");
   }
-  postUpdate(){
-    firebase.firestore().collection("posts").add({
-      text: this.text,
-      created: firebase.firestore.FieldValue.serverTimestamp(),
-      owner: firebase.auth().currentUser.uid,
-      owner_name:firebase.auth().currentUser.displayName
-    }).then((doc) => {
-      console.log(doc);
-      this.getPosts();
-    }).catch((err) =>{
-      console.log(err);
+  postUpdate() {
+    firebase
+      .firestore()
+      .collection("posts")
+      .add({
+        text: this.text,
+        created: firebase.firestore.FieldValue.serverTimestamp(),
+        owner: firebase.auth().currentUser.uid,
+        owner_name: firebase.auth().currentUser.displayName
+      })
+      .then(doc => {
+        console.log(doc);
+        this.getPosts();
+      })
       
-    })
+      .catch(err => {
+        console.log(err);
+      });
   }
-  logOut(){
+  logOut() {
     this.navCtrl.push(LoginPage);
   }
 
-  timeStamp(time){
+  timeStamp(time) {
     let timeDiff = moment(time).diff(moment());
     return moment.duration(timeDiff).humanize();
+  }
+  loadMorePosts(event){
+    firebase
+      .firestore()
+      .collection("posts")
+      .orderBy("created", "desc")
+      .startAfter(this.cursor)
+      .limit(this.pageSize)
+      .get()
+      .then(data => {
+        // re-arrange posts by descending order
+
+        data.forEach(document => {
+          this.posts.push(document);
+        });
+        console.log(this.posts);
+        if(data.size < this.pageSize){
+          //all data from firestore have been loaded 
+          event.enable(false);
+        }else{
+          event.complete();
+          this.cursor = this.posts[this.posts.length -1];
+
+        }
+      })
+      .catch(err => {
+        console.log(err);
+      });
 
   }
-
 }
